b07b8791a8d3454421450dd36ceb29f7bb4f5260
Niespodzianek <64963974+Niespodzianek@users.noreply.github.com>
2025-07-17 20:28:34 +0000

    Pierwsze listy spółek

--- analizator.py
+++ analizator.py
@@ -1,4 +1,5 @@
 from obsluga import naglowek, nacisnij_enter_aby_kontynuowac, wyczysc_ekran
+from lista_spolek import wig
 
 def analiza(verbose) -> None:
     if verbose:
@@ -7,13 +8,14 @@ def analiza(verbose) -> None:
     wyczysc_ekran()
     naglowek()
     print("Analizator spółek")
-    nacisnij_enter_aby_kontynuowac()
+    
     # pobranie danych surowych z API
+    print(wig)
     # zapis danych surowych do pliku
     # szukanie błędów
     # wyczyszczenie danych
     # zapis danych wyczyszczonych do pliku
     # uzupełnieni danych o wskaźniki
     # zapis danych uzupełnionych do pliku
-    
+    nacisnij_enter_aby_kontynuowac()
     return 0
---
+++ lista_spolek.py
@@ -0,0 +1,5 @@
+wig: list = ["CBF.WA"]
+
+nasdaq: list = ["AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "NVDA", "META"]
+
+pozostale: list = ["ROG.SW"]
